6b4e8d9324761de070778513f89249dd25144919
luistiagofavas <si_ul_4@hotmail.com>
2021-05-25 17:12:56 +0100

    first commmit

---
+++ src/main/java/com/example/blogentrymanager/BlogentrymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogentrymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogentrymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogentrymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogentrymanager/BlogentrymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogentrymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogentrymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
